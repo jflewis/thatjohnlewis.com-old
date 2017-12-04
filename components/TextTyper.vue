66ac76abce2438c3e27cda68bea7f225ea8193d8
John Lewis <lewisjnerd@gmail.com>
2017-12-03 19:59:16 -0500

    Update TextTyper component to use computed lifecycle.
    Manipulating the DOM by hand was a silly mistake. Make use of the components data
    for the string manipulation. Start the "typing" once the component was mounted on the client.

--- components/TextTyper.vue
+++ components/TextTyper.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span id="text-typer"></span>{{type}}<span id="cursor">_</span>
+    <span id="text-typer"></span>{{typing}}<span id="cursor">_</span>
   </div>
 </template>
 
@@ -10,64 +10,48 @@ export default {
 
   data () {
     return {
-      myTitles: this.titles
-    }
-  },
-  computed: {
-    type () {
-      if (typeof document !== 'undefined') {
-        let titlesIndex = 0
-
-        const deleteWord = () => {
-          this.deleteInterval = setInterval(() => {
-            const element = document.querySelector('#text-typer')
-            element.textContent = element.textContent.substring(
-              0,
-              element.textContent.length - 1
-            )
-
-            if (!element.textContent) {
-              clearInterval(this.deleteInterval)
-              writeWord(element)
-            }
-          }, 80)
-        }
-
-        const writeWord = element => {
-          if (titlesIndex >= this.myTitles.length) {
-            titlesIndex = 0
-          }
-
-          let newWord = this.myTitles[titlesIndex]
-          let newWordIndex = 0
-          this.writeInterval = setInterval(() => {
-            if (!element) {
-              // todo: come back and fix this
-              // If used in a SPA this breaks when a second component
-              clearInterval(this)
-              return
-            }
-
-            if (newWord === element.textContent) {
-              ++titlesIndex
-              clearInterval(this.writeInterval)
-              setTimeout(deleteWord, 3000)
-              return
-            }
-            element.textContent += newWord[newWordIndex]
-            ++newWordIndex
-          }, 100)
-        }
-
-        deleteWord()
-      }
+      myTitles: this.titles,
+      typing: this.titles[0]
     }
   },
   mounted () {
-    const element = document.querySelector('#text-typer')
-    element.textContent = ''
-  },
+    let titlesIndex = 0
 
+    const deleteWord = () => {
+      this.deleteInterval = setInterval(() => {
+        this.typing = this.typing.substring(
+          0,
+          this.typing.length - 1
+        )
+
+        if (!this.typing) {
+          clearInterval(this.deleteInterval)
+          writeWord()
+        }
+      }, 80)
+    }
+
+    const writeWord = () => {
+      if (titlesIndex >= this.myTitles.length) {
+        titlesIndex = 0
+      }
+
+      let newWord = this.myTitles[titlesIndex]
+      let newWordIndex = 0
+      this.writeInterval = setInterval(() => {
+        if (newWord === this.typing) {
+          ++titlesIndex
+          clearInterval(this.writeInterval)
+          setTimeout(deleteWord, 3000)
+          return
+        }
+        this.typing += newWord[newWordIndex]
+        ++newWordIndex
+      }, 100)
+    }
+
+    setTimeout(deleteWord, 2000)
+  },
   beforeDestroy () {
     if (this.deleteInterval) {
       clearInterval(this.deleteInterval)
