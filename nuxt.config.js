2ac15199489a0d804abfd8a72025b309df4dbe77
John Lewis <lewisjnerd@gmail.com>
2017-12-03 20:01:39 -0500

    update HID

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Never stop growing' }
+      { hid: 'description', name: 'description', content: 'John Lewis -- Developer at Large' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
