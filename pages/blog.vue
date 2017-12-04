8bf72565fd38472f30da633f222fec1ead5ff39e
John Lewis <lewisjnerd@gmail.com>
2017-12-03 20:01:55 -0500

    Blog building blocks and run yarn install.
    Netlify uses Yarn if it detects it. I should use it instead.

--- pages/blog.vue
+++ pages/blog.vue
@@ -11,5 +11,26 @@
 </template>
 
 <script>
+// import { createClient } from '~/plugins/contentful.js'
+// import VueMarkdown from 'vue-markdown'
 
+// const client = createClient()
+
+export default {
+  // `env` is available in the context object
+  // asyncData ({ env }) {
+  //   return client
+  //     .getEntries({
+  //       content_type: env.CTF_BLOG_POST_TYPE_ID,
+  //       order: '-sys.createdAt'
+  //     })
+  //     .then(posts => {
+  //       return { posts: posts.items }
+  //     })
+  //     .catch(console.error)
+  // },
+  // components: {
+  //   VueMarkdown
+  // }
+}
 </script>
